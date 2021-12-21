a7edf77f77ff47c3a804f1055c7fa67bf522b9be
Sail <l.cheungece@gmail.com>
2021-12-21 03:40:28 -0800

    Added hamburger menu for mobile devices

--- src/App.tsx
+++ src/App.tsx
@@ -15,14 +15,27 @@ const App = () => {
     AOS.refresh();
   }, []);
 
+  function showNavigation() {
+    const mobileNavigation = document.getElementById("mobile-navi")
+    mobileNavigation.classList.toggle("hidden")
+  }
+
   return (
     <div className="min-w-full min-h-screen bg-gray-100">
-      <header className="min-w-full p-4 text-white fixed font-bold bg-gray-100 z-10">
-        <h1 className="float-left">lischeu</h1>
-        <nav className="float-right text-2xl space-x-4 text-rose">
+      <header className="flex flex-wrap gap-y-2 p-4 justify-between min-w-full text-white fixed font-bold bg-gray-100 z-10 lowercase">
+
+        <h1 className="">Portfolio</h1>
+        <nav className="text-2xl space-x-4 text-rose hidden md:block">
           <a href="#about" className="hover:text-salmon">About</a>
           <a href="#projects" className="hover:text-salmon">Projects</a>
         </nav>
+        <nav className="text-2xl text-rose block transform -rotate-90 select-none md:hidden"><span onClick={showNavigation}>|||</span>
+        </nav>
+        <div className="basis-full min-w-full h-0 block md:hidden"></div>
+        <div id="mobile-navi" className="p-2 min-w-full border-solid text-xl text-center text-rose hidden xl:hidden h-max md:h-0">
+          <span className="hover:text-salmon"><a href="#about">About</a></span><br />
+          <a href="#projects" className="hover:text-salmon">Projects</a>
+        </div>
       </header>
       <section id="about">
         <About />
@@ -31,9 +44,9 @@ const App = () => {
         <Projects />
       </section>
       <footer className="text-center p-4">
-        &copy; {year} lischeu
+        &copy; {year} Lisa Cheung
       </footer>
-    </div>
+    </div >
   )
 }
 
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ const About = () => {
         <div className="w-3/4 rounded p-4 space-y-4 bg-dust md:w-1/2">
           <h2>Hi, I'm Lisa!</h2>
           <p>
-            I'm a computer engineering graduate originally from Southern California who's self-motivated and always learning new technologies! In my free time, I enjoy making silly animations and video games.
+            I graduated from Cal Poly Pomona with a bachelors in Computer Engineering and am always learning new technologies! In my free time, I enjoy making silly animations and video games.
           </p>
           <div className="space-x-2 space-y-2">
             <button className="bg-rose hover:bg-salmon active:bg-salmon text-gray-100 font-bold py-2 px-4 rounded">
